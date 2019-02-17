d45c824bd4acd1a213dfa9f9cefe2452a6a45fdf
Ahmad Luqman <ahmad.luqman@gmail.com>
2019-02-17 14:43:38 +0500

    fixing issues copying from original code. Still getting issues

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,13 +1,13 @@
 import { RECEIVE_TWEETS } from '../actions/tweets'
 
-export default function tweets(state = {}, action) {
+export default function tweets (state = {}, action) {
   switch(action.type) {
-    case RECEIVE_TWEETS:
+    case RECEIVE_TWEETS :
       return {
         ...state,
         ...action.tweets
       }
-    default:
+    default :
       return state
   }
 }
